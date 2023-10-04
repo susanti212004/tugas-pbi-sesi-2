15c285929d7b8c8eecfb35ab83b3675e98bc58b4
susanti212004 <123722597+susanti212004@users.noreply.github.com>
2023-10-04 14:21:51 +0700

    Add files via upload

---
+++ tugas sesi2.js
@@ -0,0 +1,9 @@
+let firstName = "Susan";
+let lastName = "ti";
+let age = 21;
+let isMarried = false
+
+console.log(" Nama depan : "+ firstName)
+console.log(" Nama belakang :"+ lastName)
+console.log(" Umur : "+ age)
+console.log("menikah : "+ isMarried)
